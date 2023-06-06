4628d0da91280274823ace7624e8971668f20292
Tomer Iwan <t.iwan@vu.nl>
2023-06-06 12:57:44 +0200

    feat: get course discussions

---
+++ R/get_discussions.R
@@ -0,0 +1,36 @@
+#' Retrieves the discussion topics within a course.
+#'
+#' This function retrieves the discussion topics within a specific course in the Canvas LMS API.
+#'
+#' @param canvas An object containing the Canvas API key and base URL, obtained through the `canvas_authenticate` function.
+#' @param course_id The ID of the course for which to fetch the discussion topics.
+#'
+#' @return A list of discussion topics within the specified course.
+#' @export
+#'
+#' @examples
+#' # Authenticate with the Canvas LMS API
+#' canvas <- canvas_authenticate(api_key, base_url)
+#'
+#' # Fetch the discussion topics of a specific course
+#' course_id <- 12345
+#' discussions <- get_discussions(canvas, course_id)
+get_discussions <- function(canvas, course_id, per_page = 100) {
+  # Construct the API endpoint URL
+  url <- paste0(canvas$base_url, "/api/v1/courses/", course_id, "/discussion_topics?per_page=", per_page)
+
+  # Make the API request
+  response <- httr::GET(url, httr::add_headers(Authorization = paste("Bearer", canvas$api_key)))
+
+  # Check the response status code
+  if (httr::status_code(response) != 200) {
+    stop("Failed to retrieve discussion topics. Please check your authentication and API endpoint.")
+  }
+
+  # Parse the response as JSON
+  discussions <- httr::content(response, "text", encoding = "UTF-8") %>%
+    jsonlite::fromJSON(flatten = TRUE)
+
+  # Return the list of discussion topics
+  return(discussions)
+}
