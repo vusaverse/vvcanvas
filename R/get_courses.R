5ba6ef117fe8f853716ed32e6186d35d4c2950b8
Tomer Iwan <t.iwan@vu.nl>
2023-06-06 12:45:32 +0200

    feat: get courses

---
+++ R/get_courses.R
@@ -0,0 +1,37 @@
+#' Get Courses from Canvas LMS API
+#'
+#' Retrieves a list of courses from the Canvas LMS API.
+#'
+#' @param canvas A list containing the 'api_key' and 'base_url' for authentication.
+#' @param per_page Number of courses to retrieve per page. Default is 100.
+#'
+#' @return A list of courses retrieved from the Canvas LMS API.
+#' @export
+#'
+#' @examples
+#' # Authenticate with the Canvas LMS API
+#' canvas <- canvas_authenticate(api_key, base_url)
+#'
+#' # Fetch all courses
+#' courses <- get_courses(canvas)
+get_courses <- function(canvas, per_page = 100) {
+  # Construct the API endpoint URL
+  url <- paste0(canvas$base_url, "/api/v1/courses?per_page=", per_page)
+
+  # Make the API request
+  response <- httr::GET(url, httr::add_headers(Authorization = paste("Bearer", canvas$api_key)))
+
+  # Check the response status code
+  if (httr::status_code(response) != 200) {
+    stop("Failed to retrieve courses. Please check your authentication and API endpoint.")
+  }
+
+  # Parse the response as JSON
+  courses <- httr::content(response, "text", encoding = "UTF-8") %>%
+    jsonlite::fromJSON(flatten = TRUE)
+
+  # Return the list of courses
+  return(courses)
+}
+
+
