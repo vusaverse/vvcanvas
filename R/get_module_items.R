27dd006b70234cbb8c2031a200c2bb566071be3e
Tomer Iwan <t.iwan@vu.nl>
2023-06-06 13:02:42 +0200

    feat: get items of course module

---
+++ R/get_module_items.R
@@ -0,0 +1,38 @@
+#' Retrieves the items within a specific module.
+#'
+#' This function retrieves the items within a specific module of a course in the Canvas LMS API.
+#'
+#' @param canvas An object containing the Canvas API key and base URL, obtained through the `canvas_authenticate` function.
+#' @param course_id The ID of the course containing the module.
+#' @param module_id The ID of the module for which to fetch the items.
+#'
+#' @return A list of items within the specified module.
+#' @export
+#'
+#' @examples
+#' # Authenticate with the Canvas LMS API
+#' canvas <- canvas_authenticate(api_key, base_url)
+#'
+#' # Fetch the items of a specific module
+#' course_id <- 12345
+#' module_id <- 67890
+#' items <- get_module_items(canvas, course_id, module_id)
+get_module_items <- function(canvas, course_id, module_id) {
+  # Construct the API endpoint URL
+  url <- paste0(canvas$base_url, "/api/v1/courses/", course_id, "/modules/", module_id, "/items")
+
+  # Make the API request
+  response <- httr::GET(url, httr::add_headers(Authorization = paste("Bearer", canvas$api_key)))
+
+  # Check the response status code
+  if (httr::status_code(response) != 200) {
+    stop("Failed to retrieve module items. Please check your authentication and API endpoint.")
+  }
+
+  # Parse the response as JSON
+  items <- httr::content(response, "text", encoding = "UTF-8") %>%
+    jsonlite::fromJSON(flatten = TRUE)
+
+  # Return the list of items
+  return(items)
+}
