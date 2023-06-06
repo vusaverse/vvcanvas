b40459f01befec63f22eae355ab44ef068ee9937
Tomer Iwan <t.iwan@vu.nl>
2023-06-06 12:53:14 +0200

    feat: get course assignments

---
+++ R/get_assignments.R
@@ -0,0 +1,36 @@
+#' Get Assignments from Canvas LMS API
+#'
+#' Fetches a list of assignments within a specific course from the Canvas LMS API.
+#'
+#' @param canvas A list containing the 'api_key' and 'base_url' for authentication.
+#' @param course_id The ID of the course for which to retrieve the assignments.
+#'
+#' @return A list of assignments retrieved from the Canvas LMS API.
+#' @export
+#'
+#' @examples
+#' # Authenticate with the Canvas LMS API
+#' canvas <- canvas_authenticate(api_key, base_url)
+#'
+#' # Fetch the list of assignments for a specific course
+#' course_id <- 12345
+#' assignments <- get_assignments(canvas, course_id)
+get_assignments <- function(canvas, course_id) {
+  # Construct the API endpoint URL
+  url <- paste0(canvas$base_url, "/api/v1/courses/", course_id, "/assignments")
+
+  # Make the API request
+  response <- httr::GET(url, httr::add_headers(Authorization = paste("Bearer", canvas$api_key)))
+
+  # Check the response status code
+  if (httr::status_code(response) != 200) {
+    stop("Failed to retrieve assignments. Please check your authentication and API endpoint.")
+  }
+
+  # Parse the response as JSON
+  assignments <- httr::content(response, "text", encoding = "UTF-8") %>%
+    jsonlite::fromJSON(flatten = TRUE)
+
+  # Return the list of assignments
+  return(assignments)
+}
