410476dea2c9d38d68e0b8666f26cb5e770e582d
Tomer Iwan <t.iwan@vu.nl>
2023-06-06 14:20:19 +0200

    feat: get details for single course

---
+++ R/get_course_details.R
@@ -0,0 +1,40 @@
+#' Get Course Details from Canvas LMS API
+#'
+#' Retrieves detailed information about a specific course from the Canvas LMS API.
+#'
+#' @param canvas A list containing the 'api_key' and 'base_url' for authentication.
+#' @param course_id The ID of the course for which to retrieve the details.
+#'
+#' @return A dataframe containing the detailed information about the course.
+#' @export
+#'
+#' @examples
+#' # Authenticate with the Canvas LMS API
+#' canvas <- canvas_authenticate(api_key, base_url)
+#'
+#' # Fetch the details of a specific course
+#' course_id <- 12345
+#' course_details <- get_course_details(canvas, course_id)
+#'
+#' # Print the course details
+#' print(course_details)
+get_course_details <- function(canvas, course_id) {
+  # Construct the API endpoint URL
+  url <- paste0(canvas$base_url, "/api/v1/courses/", course_id)
+
+  # Make the API request
+  response <- httr::GET(url, httr::add_headers(Authorization = paste("Bearer", canvas$api_key)))
+
+  # Check the response status code
+  if (httr::status_code(response) != 200) {
+    stop("Failed to retrieve course details. Please check your authentication and API endpoint.")
+  }
+
+  # Parse the response as JSON and convert to a dataframe
+  course_details <- httr::content(response, "text", encoding = "UTF-8") %>%
+    jsonlite::fromJSON(flatten = TRUE) %>%
+    dplyr::bind_rows()
+
+  # Return the course details dataframe
+  return(course_details)
+}
