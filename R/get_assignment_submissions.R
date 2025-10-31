458cc77390c0eb05dc82b6dc583e6dbf7b99db6c
listenm <n.smits@uva.nl>
2025-10-31 15:53:31 +0100

    improvement of get_assignment_submissions

--- R/get_assignment_submissions.R
+++ R/get_assignment_submissions.R
@@ -1,25 +1,29 @@
-#' Lists assignment submissions for a course.
+#' Retrieves assignment submissions.
 #'
-#' This function retrieves a list of assignment submissions for a specific course in the Canvas LMS API.
+#' This function retrieves the submissions to a specific assignment in a specific course in the
+#' Canvas LMS API.
 #'
 #' @param canvas An object containing the Canvas API key and base URL, obtained through the `canvas_authenticate` function.
-#' @param course_id The ID of the course for which to list assignment submissions.
-#' @param assignment_id (Optional) The ID of a specific assignment for which to list submissions.
+#' @param course_id The ID of the course for which to fetch submissions.
+#' @param assignment_id The ID of the assignment for which to fetch the submissions.
 #' @param per_page (Optional) The number of submissions to retrieve per page of results (default is 100).
 #'
-#' @return A data frame of assignment submissions for the specified course and assignment.
+#' @return A data frame containing the submission data.
 #' @export
-get_assignment_submissions <- function(canvas, course_id, assignment_id = NULL, per_page = 100) {
+get_assignment_submissions <- function (canvas, course_id, assignment_id, per_page = 100)
+{
   # Construct the API endpoint URL
-  url <- paste0(canvas$base_url, "/api/v1/courses/", course_id, "/students/submissions?per_page=", per_page)
-
-  # Append the assignment_id if provided
-  if (!is.null(assignment_id)) {
-    url <- paste0(url, "&assignment_id=", assignment_id)
-  }
+  url <- paste0(canvas$base_url, "/api/v1/courses/", course_id, "/assignments/",
+    assignment_id, "/submissions?per_page=", per_page)
 
   # Make the API request
   response <- httr::GET(url, httr::add_headers(Authorization = paste("Bearer", canvas$api_key)))
+
+  # Check the response status code
+  if (httr::status_code(response) != 200) {
+    stop("Failed to retrieve assignment submissions. Please check your authentication and API
+    endpoint.")
+  }
 
   # Use pagination helper to get all pages
   responses <- paginate(response, canvas$api_key)
@@ -27,8 +31,8 @@ get_assignment_submissions <- function(canvas, course_id, assignment_id = NULL, 
   # Parse and combine all results
   submissions_list <- lapply(responses, function(resp) {
     httr::content(resp, "text", encoding = "UTF-8") %>%
-      jsonlite::fromJSON(flatten = TRUE) %>%
-      as.data.frame()
+    jsonlite::fromJSON(flatten = TRUE) %>%
+    as.data.frame()
   })
   submissions <- dplyr::bind_rows(submissions_list)
 
